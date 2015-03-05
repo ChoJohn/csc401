f83870c655156a03cfc7c88760d75c8034ba23d7
Friedman Tal <g3friedm@cdf.toronto.edu>
2015-03-05 18:18:32 -0500

    fixed a bug

--- A2/load_test.m
+++ A2/load_test.m
@@ -1,11 +1,10 @@
-[eng, fre] = load_test(filename)
+function [eng, fre] = load_test(filename)
 	eng = {};
 	fre = {}; 
 	elines = textread(strcat(filename, '.e'), '%s', 'delimiter', '\n');
 	flines = textread(strcat(filename, '.f'), '%s', 'delimiter', '\n');
 
 	for i=1:length(elines)
-		eng{i} = strsplit(' ', preprocess(elines{i}, 'e'));
-		fre{i} = strsplit(' ', preprocess(flines{i}, 'f'));
+		eng{i} = preprocess(elines{i}, 'e');
+		fre{i} = preprocess(flines{i}, 'f');
 	end
-end
