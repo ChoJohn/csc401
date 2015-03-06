713af4c6151a8107f7dab96817b582830429b969
Tal Friedman <Tal Friedman>
2015-03-05 19:06:20 -0500

    First pass part 5 done

--- A2/evalAlign.m
+++ A2/evalAlign.m
@@ -7,70 +7,38 @@
 % some of your definitions
 trainDir     = '/u/cs401/A2_SMT/data/Hansard/Training/';
 testDir      = '/u/cs401/A2_SMT/data/Hansard/Testing/';
-fn_LME       = TODO;
-fn_LMF       = TODO;
-numSentences = TODO;
+fn_LME       = '~/LM_e';
+fn_LMF       = '~/LM_f';
 
 % Train your language models. This is task 2 which makes use of task 1
 %LME = lm_train( trainDir, 'e', fn_LME );
 %LMF = lm_train( trainDir, 'f', fn_LMF );
-% Load in previously trained language models
-LME = load('~/out.lm', '-mat');
-LME = LME.LM;
-LMF = load('~/outf.lm', '-mat');
-LMF = LMF.LM;
 
 % Train your alignment model of French, given English 
 %AMFE = align_ibm1( trainDir, numSentences );
 % ... TODO: more 
-% Load our alignment models
+% This is the code that would create the models I used, actally training them would take ~15 hours
+
+%sizes = {1000, 10000, 15000, 30000, 100000};
+%am = {};
+%for i=1:5
+	%am{i} = align_ibm1('/u/cs401/A2_SMT/data/Hansard/Training/', sizes{i}, 10, strcat('~/AM_', int2str(sizes{i}/1000), 'K'));
+%end
 
 % TODO: a bit more work to grab the English and French sentences. 
 %       You can probably reuse your previous code for this  
-
-% Decode the test sentence 'fre'
-eng = decode2( fre, LME, AMFE, '');
+[eng, fre] = load_test('/u/cs401/A2_SMT/data/Hansard/Testing/Task5');
 
 % TODO: perform some analysis
-
-
-
-
-
-%%%%%% Helper functions %%%%%%%%%%
-
-function [eng, fre] = read_hansard(mydir, numSentences)
-%
-% Read 'numSentences' parallel sentences from texts in the 'dir' directory.
-%
-% Important: Be sure to preprocess those texts!
-%
-% Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f
-% You can decide what form variables 'eng' and 'fre' take, although it may be easiest
-% if both 'eng' and 'fre' are cell-arrays of cell-arrays, where the i^th element of 
-% 'eng', for example, is a cell-array of words that you can produce with
-%
-%         eng{i} = strsplit(' ', preprocess(english_sentence, 'e'));
-%
-  eng = {};
-  fre = {};
-
-  % TODO: your code goes here.
-  % Get list of english and french files they will be aligned as the OS returns alphabetical order)
-  DE = dir([mydir, filesep, '*', 'e']);
-  DF = dir([mydir, filesep, '*', 'f']);
-  linecount = 1;
-  for iFile=1:length(DE)
-	elines = textread([testDir, filesep, DE(iFile).name], '%s','delimiter','\n');
-    flines = textread([testDir, filesep, DF(iFile).name], '%s','delimiter','\n');
-	for i=1:length(elines)
-		eng{linecount} = strsplit(' ', preprocess(elines{i}, 'e'));
-		fre{linecount} = strsplit(' ', preprocess(flines{i}, 'f'));
-		linecount = linecount + 1;
-		if linecount > numSentences
-			return
-		end
+prop = {}
+for model=1:length(am)
+	total = 0;
+	correct = 0;
+	for i=1:length(fre)
+		trans = decode(fre{i}, LME, am{model}, '');
+		correct = correct + score_align(trans, eng{i});
+		total = total + length(trans) - 2;
 	end
-  end
-
+	prop{model} = correct / total;
 end
+
