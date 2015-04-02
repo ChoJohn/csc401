9ceffd42cf33f72babedf632d13eea42bd8c4d6b
Tal Friedman <talf301@gmail.com>
2015-04-02 14:05:15 -0400

    Fixed up some em stuff

--- A3/comp_b_ll.m
+++ A3/comp_b_ll.m
@@ -1,10 +1,11 @@
-function b = comp_b(mfcc, M, gmm)
-% Helper functions which computes TxM matrix of log(b_m(x_t))'s
+function [b, ll] = comp_b_ll(mfcc, M, gmm)
+% Helper functions which computes TxM matrix of log(b_m(x_t))'s and log likelihood
 % input: mfcc: concatenated training data for user
 %        M: number of components in model
 %		 gmm: The appropriate struct for the current speaker's model
 
 % output: b: TxM matrix of log(b_m(x_t))'s
+%          ll: log likelihood of data under model
 
 % D is number of dimensions, T is number of training cases
   D = size(mfcc,2);
@@ -12,7 +13,7 @@ function b = comp_b(mfcc, M, gmm)
 
 % First compute log of numerator
   num = zeros(T, M);
-  for i=1:dims
+  for i=1:D
 	num = num + (repmat(mfcc(:,i),1,M) - repmat(gmm.means(i,:),T,1)).^2 ./ squeeze(repmat(gmm.cov(i,i,:),1,T,1));
   end
   num = -1/2 * num;
@@ -25,6 +26,12 @@ function b = comp_b(mfcc, M, gmm)
 
   % Log of denominator
   den = D/2*log(2*pi) + 1/2*repmat(sum(log(comp_covs),1),T,1);
+
+  % Compute b
   b = num - den;
+
+  % Compute log likelihood
+  wb = gmm.weights * exp(b)';
+  ll = sum(log(wb), 2);
 
 end
--- A3/gmmTrain.m
+++ A3/gmmTrain.m
@@ -25,12 +25,12 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 	 continue
 	end	 
 	curr_speak = curr_speak + 1;
-	gmms{curr_speak} = struct()
-	gmms{curr_speak}.name = speakers(i).name
+	gmms{curr_speak} = struct();
+	gmms{curr_speak}.name = speakers(i).name;
 	mfccs = dir(strcat(dir_train, '/', speakers(i).name, '/*.mfcc'));
 	mfcc = [];
 	for j=1:size(mfccs,1)
-	  to_load = strcat(dir_train, '/', speakers(i).name, '/', mfccs(i).name);
+	  to_load = strcat(dir_train, '/', speakers(i).name, '/', mfccs(j).name);
 	  single = load(to_load);
 	  mfcc = [mfcc; single];
 	end
@@ -46,27 +46,30 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 	% Do actual training
 	prev_L = -Inf;
 	improvement = Inf;
-	i = 0;
-	while i <= max_iter && improvement > epsilon do
+	j = 0;
+	while j <= max_iter && improvement > epsilon
       [mus, sigmas, omegas, ll] = em_step(mfcc, M, gmms{curr_speak});
 	  gmms{curr_speak}.weights = omegas;
 	  gmms{curr_speak}.means = mus;
 	  gmms{curr_speak}.cov = sigmas;
 	  improvement = ll - prev_L;
 	  prev_L = ll;
-	  i = i+1;
-	end_while
+	  i
+	  j
+	  ll
+	  j = j+1;
+	end
   end
 end
 
 function [mus, sigmas, omegas] = initialize(mfcc, M)
   % Initialize mus to random lines from the data
   % (data is already shuffled)
-  dims = size(mfcc,2);
-  mus = mfcc[1:M, :]';
+  D = size(mfcc,2);
+  mus = mfcc(1:M, :)';
   % Initialize omegas to 1/M, sigmas to identity
   omegas = 1/M * ones(1,M);
-  sigmas = repmat(eye(dim),1,1,M);
+  sigmas = repmat(eye(D),1,1,M);
 end
 
 function [mus, sigmas, omegas, ll] = em_step(mfcc, M, gmm)
@@ -74,11 +77,9 @@ function [mus, sigmas, omegas, ll] = em_step(mfcc, M, gmm)
   D = size(mfcc,2);
   T = size(mfcc,1);
   % First do E-step, compute conditional probability and ll
-  % First, compute our TxM matrix of b's
-  b = comp_b(mfcc, M, gmm);  
 
-  % Now compute log likelihood using b's
-  ll = comp_ll(b, gmm, mfcc);
+  % First, compute our TxM matrix of b's and ll
+  [b, ll] = comp_b_ll(mfcc, M, gmm);
   
   % Compute conditional probabilities p(m|x_t,theta) so we can use it in M-step
   wb = repmat(gmm.weights,T,1) .* exp(b);
@@ -90,7 +91,7 @@ function [mus, sigmas, omegas, ll] = em_step(mfcc, M, gmm)
   omegas = sum_t_conds / T;
   mus = mfcc' * conds ./ repmat(sum_t_conds, D, 1);
   % We will have to expand sigmas to diagonals, but first compute it like this
-  comp_sigmas = ((mfcc.^2)' * conds ./ repmat(sum_t_conds, D, 1)) - mus.^2
+  comp_sigmas = ((mfcc.^2)' * conds ./ repmat(sum_t_conds, D, 1)) - mus.^2;
   sigmas = zeros(D,D,M);
   % Expand them
   for i=1:M
